70c1bf5ba7c1ff82e24d17fcf062f948303c4839
wickedrun <hoon3284@icloud.com>
2023-06-16 18:09:21 +0900

    EntryPoint에서 CounterFeature 생성 및 제공 구현

--- Targets/TCAExample/Sources/TCAExampleApp.swift
+++ Targets/TCAExample/Sources/TCAExampleApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
 
+import ComposableArchitecture
+
 @main
 struct TCAExampleApp: App {
+    // 응용프로그램을 구동하는 Store는 한 번만 만들어야 한다는 점에 유의해야 합니다.
+    // 대부분의 경우 WindowGroup에서 직접 만들어도 충분하지만,
+    // 정적 변수로 유지한 다음 WindowGroup에서 넣어줄 수도 있습니다.
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCAExampleApp.store)
         }
     }
 }
