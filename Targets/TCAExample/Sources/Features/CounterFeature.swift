f2814ce619737d5a344f121470ab56dbed8426e0
wickedrun <hoon3284@icloud.com>
2023-06-16 19:28:05 +0900

    CounterFeature에 Timer 동작하도록 구현

--- Targets/TCAExample/Sources/Features/CounterFeature.swift
+++ Targets/TCAExample/Sources/Features/CounterFeature.swift
@@ -15,6 +15,7 @@ struct CounterFeature: ReducerProtocol {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
@@ -22,7 +23,11 @@ struct CounterFeature: ReducerProtocol {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
@@ -56,6 +61,26 @@ struct CounterFeature: ReducerProtocol {
             state.count += 1
             state.fact = nil
             return .none
+            
+        case .timerTick:
+            state.count += 1
+            state.fact = nil
+            return .none
+            
+        case .toggleTimerButtonTapped:
+            state.isTimerRunning.toggle()
+            
+            if state.isTimerRunning {
+                return .run { send in
+                    while true {
+                        try await Task.sleep(for: .seconds(1))
+                        await send(.timerTick)
+                    }
+                }
+                .cancellable(id: CancelID.timer)
+            } else {
+                return .cancel(id: CancelID.timer)
+            }
         }
     }
     
