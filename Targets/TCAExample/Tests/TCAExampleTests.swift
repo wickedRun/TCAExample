6d9c3da333007ed669e29d682712d3be2a9a9d19
wickedrun <hoon3284@icloud.com>
2023-06-17 14:59:02 +0900

    TCAExampleTests.swift 추가

---
+++ Targets/TCAExample/Tests/TCAExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  TCAExampleTests.swift
+//  TCAExampleTests
+//
+//  Created by wickedRun on 2023/06/17.
+//  Copyright © 2023 org.wickedrun. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+final class TCAExampleTests: XCTestCase {
+    func test_twoPlusTwo_isFour() {
+        XCTAssertEqual(2+2, 4)
+    }
+}
