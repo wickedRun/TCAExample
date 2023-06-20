a3af1efe08dce48c38ee59fdb1d036c82b6272ea
wickedrun <hoon3284@icloud.com>
2023-06-20 17:16:41 +0900

    Delegate Action 구현

--- Targets/TCAExample/Sources/ContactsFeature/AddContactFeature.swift
+++ Targets/TCAExample/Sources/ContactsFeature/AddContactFeature.swift
@@ -15,17 +15,26 @@ struct AddContactFeature: ReducerProtocol {
     
     enum Action: Equatable {
         case cancelButtonTapped
+        case delegate(Delegate)
         case saveButtonTapped
         case setName(String)
+        
+        enum Delegate: Equatable {
+            case cancel
+            case saveContact(Contact)
+        }
     }
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
         case .cancelButtonTapped:
+            return .send(.delegate(.cancel))
+            
+        case .delegate:
             return .none
             
         case .saveButtonTapped:
-            return .none
+            return .send(.delegate(.saveContact(state.contact)))
             
         case let .setName(name):
             state.contact.name = name
