9d1253b69f389e138546f0f161364c1e5faf8b6e
wickedrun <hoon3284@icloud.com>
2023-06-20 16:36:04 +0900

    ContactsFeature 구현

---
+++ Targets/TCAExample/Sources/ContactsFeature/ContactsFeature.swift
@@ -0,0 +1,58 @@
+//
+//  ContactsFeature.swift
+//  TCAExample
+//
+//  Created by wickedRun on 2023/06/19.
+//  Copyright © 2023 org.wickedrun. All rights reserved.
+//
+
+import Foundation
+import ComposableArchitecture
+import CasePaths
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+struct ContactsFeature: ReducerProtocol {
+    struct State: Equatable {
+        @PresentationState var addContact: AddContactFeature.State?
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+    
+    enum Action: Equatable {
+        case addButtonTapped
+        case addContact(PresentationAction<AddContactFeature.Action>)
+    }
+    
+    var body: some ReducerProtocolOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                state.addContact = AddContactFeature.State(
+                    contact: Contact(id: UUID(), name: "")
+                )
+                
+                return .none
+                
+            case .addContact(.presented(.cancelButtonTapped)):
+                state.addContact = nil
+                return .none
+                
+            case .addContact(.presented(.saveButtonTapped)):
+                guard let contact = state.addContact?.contact
+                else { return .none }
+                state.contacts.append(contact)
+                state.addContact = nil
+                return .none
+                
+            case .addContact:
+                return .none
+            }
+        }
+        .ifLet(\.$addContact, action: /Action.addContact) {
+            AddContactFeature()
+        }
+    }
+}
